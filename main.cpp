8fd62ab2f43922c8e1d03a3b3ed32b0e9a870018
CubingBadWolf <gaming1dragons@gmail.com>
2023-02-10 16:17:17 +1300

    Comments for Mr Jones

--- main.cpp
+++ main.cpp
@@ -7,36 +7,42 @@ using std::vector,std::string;
 
 class Board{
 private:
-    char BoardOutputs[3] = {'_','|',' '};
-    int size;
+    char BoardOutputs[3] = {'_','|',' '}; // Grid characters
+    int size; // dimensions of square board
 public:
     Board(int Size){
+        //Class constructor
         size = Size;
     }
-    vector<int> ShipPositions(){
-
+    
+    vector<vector<int>> ShipPositions(){
+        //Will be a function to place ships
+        vector<vector<int>> coords;
+        return coords;
     }
+
     void printRow(int out){
+        //prints the individual subrows of the gameboard
         int space = 0;
-        for (int i = 0; i < 3; i++){ 
-            while(space < size){
-                std::cout << string(3, BoardOutputs[out]);
-                if (space != size-1){
-                    std::cout << BoardOutputs[1];
-                }
-                space++;
+        while(space < size){
+            std::cout << string(3, BoardOutputs[out]);
+            if (space != size-1){ // If its the final row of the board it doesn't need the closing |
+                std::cout << BoardOutputs[1]; 
             }
+            space++;
         }
+    
         std::cout << std::endl;
 
     }
     void printGameRow(){
+        //Prints the subrows into a game row
         printRow(2);
         printRow(2);
         printRow(0);
     }
     void printBoard(){
-    
+        //prints all the rows into a board
         int row = 0;
         while(row < size-1){
             printGameRow();
