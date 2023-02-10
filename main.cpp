8a1108e8af6a5a34e0a1ea5153b296cd6a629901
CubingBadWolf <gaming1dragons@gmail.com>
2023-02-10 18:50:14 +1300

    Allow inner value of board to have custom value defined in function

--- main.cpp
+++ main.cpp
@@ -9,16 +9,24 @@ class Board{
 private:
     char BoardOutputs[3] = {'_','|',' '}; // Grid characters
     int size; // dimensions of square board
+    string ListOfShips[5][2] = {{"Carrier", "5"},{"Battleship", "4"},{"Submarine","3"},{"Cruiser","3"},{"Destroyer","2"}};
+
 public:
     Board(int Size){
         //Class constructor
         size = Size;
     }
-    
-    vector<vector<int>> ShipPositions(){
-        //Will be a function to place ships
-        vector<vector<int>> coords;
-        return coords;
+    vector<vector<char>> gameboard;
+    void ShipPlacer(){
+        vector<vector<char>> backendBoard(size, vector<char>(size));
+        for (int i = 0; i < size; i++){
+            for (int j = 0; j < size; j++){
+                backendBoard[i][j] = '0';
+            }
+        } //Creates a 2 dimensional vector with blank chars inside it
+        gameboard = backendBoard;
+        
+
     }
 
     void printRow(int out){
@@ -35,21 +43,35 @@ public:
         std::cout << std::endl;
 
     }
-    void printGameRow(){
+    void printMiddleRow(vector<vector<char>> ValueBoard, int rowNum){
+        int space = 0;
+        int collumn = 0;
+        while(space < size){
+            std::cout << ' '<< ValueBoard[rowNum][collumn] << ' ';
+            if (space != size-1){ // If its the final row of the board it doesn't need the closing |
+                std::cout << BoardOutputs[1]; 
+            }
+            space++;
+            collumn++;
+        }
+    
+        std::cout << std::endl;
+    }
+    void printGameRow(int rownNum){
         //Prints the subrows into a game row
         printRow(2);
-        printRow(2);
+        printMiddleRow(gameboard,rownNum);
         printRow(0);
     }
     void printBoard(){
         //prints all the rows into a board
         int row = 0;
         while(row < size-1){
-            printGameRow();
+            printGameRow(row);
             row++;
         }
         printRow(2);
-        printRow(2);
+        printMiddleRow(gameboard,row);
         printRow(2);
     }
 
@@ -60,6 +82,7 @@ int main(){
     int size;
     size = verifyInputs("How big do you want the board to be?");
     Board Player1 = Board(size);
+    Player1.ShipPlacer();
     Player1.printBoard();
     system("pause");
     return 0;
