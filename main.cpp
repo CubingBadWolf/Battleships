818f2b28d419515a7de9b32c1dc23b86953ad605
CubingBadWolf <gaming1dragons@gmail.com>
2023-02-15 15:17:52 -0800

    Ships can be placed

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@ class Board{
 private:
     char BoardOutputs[3] = {'_','|',' '}; // Grid characters
     int size; // dimensions of square board
-    string ListOfShips[5][2] = {{"Carrier", "5"},{"Battleship", "4"},{"Submarine","3"},{"Cruiser","3"},{"Destroyer","2"}};
 
 protected:
     void printRow(int out){
@@ -46,34 +45,88 @@ protected:
         printRow(0);
     }
 public:
+    vector<vector<string>> ListOfShips = {{"Carrier", "C", "5"},{"Battleship", "B", "4"},{"Submarine", "S","3"},{"Cruiser", "c", "3"},{"Destroyer", "D", "2"}};
     Board(int Size){
         //Class constructor
         size = Size;
     }
     vector<vector<char>> gameboard;
     vector<vector<char>> playboard;
-    void ShipPlacer(){
+    void BoardInit(){
         vector<vector<char>> backendBoard(size, vector<char>(size));
         for (int q = 0; q < 2; q++){  
             for (int i = 0; i < size; i++){
                 for (int j = 0; j < size; j++){
-                    backendBoard[i][j] = '0';
+                    backendBoard[i][j] = ' ';
                 }
             } //Creates a 2 dimensional vector with blank chars inside it
             if(q == 0){
                 gameboard = backendBoard;
-                gameboard[size/2][size/2] = '*';//showing single element can be changed in one board.
 
             }
             else{
                 playboard = backendBoard;
             }
         } // Initalised the two boards in the class.
-
-
     }
 
-    
+    int PlaceShip(vector<string> ship, int direction){ //direction keys = -2 down, 2 up, -1 left, 1 right
+        int xval;int yval;
+        while (true){
+            xval = verifyInputs("Enter the x coordinate to begin placing: ");
+            if (xval < 0 || xval > size -1){
+                std::cout << "Please enter a valid number" << std::endl;
+            }
+            else{
+                break;
+            }
+        }
+        while (true){
+            yval = verifyInputs("Enter the y coordinate to begin placing: ");
+            if (yval < 0 || yval > size -1){
+                std::cout << "Please enter a valid number" << std::endl;
+            }
+            else{
+                break;
+            }
+        }
+
+        if (direction % 2 == 0){
+            if (yval -(direction/2 * std::stoi(ship[2])) < 0){
+                std::cout << "Ship cannot be placed at those coords with that direction, please try again" << std::endl;
+                return 0;
+                }                 
+
+            for(int i = 0; i <std::stoi(ship[2]); i++){
+                if (direction < 0){
+                    gameboard[yval+i][xval] = char(ship[1][0]);
+                }
+                else{
+                    gameboard[yval-i][xval] = char(ship[1][0]);
+                }
+            }
+        }
+        else{
+            try{
+                if (xval +(direction/2 * std::stoi(ship[2]) < 0)){
+                    throw(418);
+                }
+            }
+            catch(...){
+                std::cout << "Ship cannot be placed at those coords with that direction, please try again" << std::endl;
+                return 0;
+            }
+            for(int i = 0; i <std::stoi(ship[2]); i++){
+                if (direction < 0){
+                    gameboard[yval][xval-i] = char(ship[1][0]);
+                }
+                else{
+                    gameboard[yval][xval-i] = char(ship[1][0]);
+                }
+            }
+        }
+       return 1;
+    }
     void printBoard(vector<vector<char>> Board){
         //prints all the rows into a board
         int row = 0;
@@ -95,11 +148,14 @@ int main(){
     Board Player1 = Board(size);
     Board Player2 = Board(size);
     
-    Player1.ShipPlacer();
+    Player1.BoardInit();
     std::cout << "GameBoard" << std::endl;
     Player1.printBoard(Player1.gameboard);
-    std::cout << "PlayingBoard" << std::endl;
-    Player1.printBoard(Player1.playboard);
+    Player1.PlaceShip(Player1.ListOfShips[0], -2);
+    Player1.printBoard(Player1.gameboard);
+
+    /*std::cout << "PlayingBoard" << std::endl;
+    Player1.printBoard(Player1.playboard);*/
 
     system("pause");
     return 0;
